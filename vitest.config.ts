c60e47b5774c9bbca519940b2c20fc1ea005fad6
eturino <eturino@eturino.com>
2024-11-05 19:20:25 +0100

    chore: fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     globals: true,
     environment: "node",
     coverage: {
+      include: ["src/**"],
       exclude: ["**/__tests__/**", "**/commitlint.config.js"],
     }
   },
